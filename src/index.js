3282717fb6a1b6935fac63ee07897c71674baddf
Gavin Kirchner <gskirchner@gmail.com>
2017-08-01 13:45:01 -0500

    highlight winning row

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css'
 
 function Square(props) {
     return (
-        <button className="square" onClick={props.onClick}>
+        <button className={"square" + (props.isWinner ? " winner" : "")} onClick={props.onClick}>
             {props.value}
         </button>
     );
@@ -23,13 +23,24 @@ class Board extends React.Component {
     }
     
     renderSquare(i) {
-        return <Square value={this.state.squares[i]} onClick={() => this.handleSquareClick(i)}/>;
+        let winner = this.state.winner;
+        let isWinner = false;
+        if (winner && winner.winningLine.includes(i)) {
+            isWinner = true;
+        }
+        return (
+            <Square 
+                value={this.state.squares[i]} 
+                onClick={() => this.handleSquareClick(i)}
+                isWinner={isWinner}
+            />
+        );
     }
 
     render() {
         let status;
         if (this.state.winner) {
-            status = 'Winner: ' + this.state.winner;
+            status = 'Winner: ' + this.state.winner.player;
         } else {
             status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
         }
@@ -131,7 +142,10 @@ function calculateWinner(squares) {
     for (let i = 0; i < lines.length; i++) {
         const [a, b, c] = lines[i];
         if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
+            return { 
+                player: squares[a],
+                winningLine: lines[i]
+            };
         }
     }
     return null;
